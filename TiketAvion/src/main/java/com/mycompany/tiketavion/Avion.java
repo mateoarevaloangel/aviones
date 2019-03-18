f96489ba0854faf9b728068b96a3d74a089cf2d2
jejedanieljejemateo <jejedanieljejemateo@gmail.com>
2019-03-18 12:20:21 -0500

    avion

---
+++ TiketAvion/src/main/java/com/mycompany/tiketavion/Avion.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.tiketavion;
+
+import java.util.HashMap;
+
+/**
+ *
+ * @author daniel
+ */
+public class Avion {
+    int id;
+    HashMap<Integer, Persona> sillas;
+}
